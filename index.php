ddc5bf8defdaa8069bdcf3c55f9599cbf3ed57c8
Kenarawich <76036891+Kenarawich@users.noreply.github.com>
2020-12-15 16:08:02 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
     <title>ทดสอบ HTML</title>
   </head>
   <body>
-          สวัสดี ศิลปากร
+    <h1>สวัสดี ศิลปากร</h1>
+    <h2>The href Attribute</h2>
+
+<p>HTML links are defined with the a tag. The link address is specified in the href attribute:</p>
+
+<a href="https://www.cp.su.ac.th/curricula/Bachelor-of-Science-Program-in-Data-Science">Visit W3Schools</a>
   </body>
 </html>
